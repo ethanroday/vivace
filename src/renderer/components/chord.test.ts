48bdd542109962c50b62cc87e9c23df677787c91
Ethan Roday <ethan.roday@gmail.com>
2018-10-10 18:39:30 -0700

    Add comments and some more tests

--- src/renderer/components/chord.test.ts
+++ src/renderer/components/chord.test.ts
@@ -24,8 +24,23 @@ describe("getNoteheadOffsets", () => {
     factory([-1, -1], [-4, -3], StemDirection.UP)
   ));
 
+  describe("triads", () => runAll(
+    factory([0, 0, 0], [3, 4, 5], StemDirection.DOWN),
+    factory([-1, -1, -1], [-4, -3, -2], StemDirection.UP)
+  ));
+
   describe("three-note clusters", () => runAll(
     factory([0, -1, 0], [3, 3.5, 4], StemDirection.DOWN),
     factory([-1, 0, -1], [-4, -3.5, -3], StemDirection.UP),
+  ));
+
+  describe("four-note clusters", () => runAll(
+    factory([-1, 0, -1, 0], [2.5, 3, 3.5, 4], StemDirection.DOWN),
+    factory([-1, 0, -1, 0], [-4.5, -4, -3.5, -3], StemDirection.UP),
+  ));
+
+  describe("two isolated clusters", () => runAll(
+    factory([-1, 0, -1, 0], [0, 0.5, 3.5, 4], StemDirection.DOWN),
+    factory([-1, 0, -1, 0], [-4.5, -4, -2.5, -2], StemDirection.UP),
   ));
 });
--- src/renderer/components/chord.ts
+++ src/renderer/components/chord.ts
@@ -1,6 +1,15 @@
 import { StemDirection, StaffPosition } from "../../models";
 import { isHalfStaffSpace } from "../../util";
 
+const OFFSETS_BY_STEM_DIRECTION = {
+  // When the stem points down, the preferred
+  // notehead offset is to the right of the stem
+  [StemDirection.DOWN]: [0, -1],
+  // When the stem points up, the preferred
+  // notehead offset is to the left of the stem
+  [StemDirection.UP]: [-1, 0],
+}
+
 /**
  * Given a list of staff positions and a stem direction, return the
  * list of offsets *relative to the stem* necessary to render the noteheads correctly.
@@ -9,26 +18,30 @@ import { isHalfStaffSpace } from "../../util";
  * @param stemDirection The direction of the stem on which these noteheads will be rendered
  */
 export const getNoteheadOffsets = (staffPositions: StaffPosition[], stemDirection: StemDirection) => {
-  const preferred = stemDirection === StemDirection.UP ? -1 : 0;
-  const reversed = stemDirection === StemDirection.UP ? 0 : -1;
+  const [preferred, reversed] = OFFSETS_BY_STEM_DIRECTION[stemDirection];
   const start = stemDirection === StemDirection.UP ? 0 : staffPositions.length - 1;
   const end = stemDirection === StemDirection.UP ? staffPositions.length - 1 : 0;
 
   const dir = end < start ? -1 : 1;
-  let left = (end - start + dir) * dir;
+  let notesLeft = (end - start + dir) * dir;
   let i = start;
   const offsets = new Array<number>(staffPositions.length);
-  while (left > 1) {
+  // Start from the end of the stem where there's a notehead
+  // and consider each pair of noteheads
+  while (notesLeft > 1) {
+    // Place the first one in the preferred position
     offsets[i] = preferred;
+    // If the pair is a half step, place the second in the reversed position
     if (isHalfStaffSpace(staffPositions[i], staffPositions[i + dir])) {
       offsets[i + dir] = reversed;
       i += dir;
-      left -= 1;
+      notesLeft -= 1;
     }
     i += dir;
-    left -= 1;
+    notesLeft -= 1;
   }
-  if (left === 1) {
+  // If we haven't placed the last note, place it in the preferred position
+  if (notesLeft === 1) {
     offsets[i] = preferred;
   }
   return offsets;
